6a4c2310d00a93b23163ffc2e36b7ef8739edb18
Zahin52 <zahinuddin5@gmail.com>
2021-05-11 15:44:04 +0600

    Version 1.0.0

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ mongoose.connect(dbURL,{useNewUrlParser:true,useUnifiedTopology:true})
         }).catch(err=>{
             console.log(err)
         })
+const port= process.env.PORT || 5000
+app.listen(port,()=>{
+    console.log(`listening at port ${port}`)
+})
 //middleware
 app.use(bodyParser.json())
 
@@ -38,7 +42,3 @@ if (process.env.NODE_ENV === "production"){
     })
 }
 
-const port= process.env.PORT || 5000
-app.listen(port,()=>{
-    console.log(`listening at port ${port}`)
-})
