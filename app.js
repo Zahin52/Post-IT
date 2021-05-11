c093c5001a964ea2da4b24c0b30d02bc70c6c8a9
Zahin52 <zahinuddin5@gmail.com>
2021-05-11 15:16:55 +0600

    Version 1.0.0

--- app.js
+++ app.js
@@ -29,12 +29,12 @@ app.get("/",(req,res)=>{
 app.use("/post",postroute)
 
 //post build scripts 
-if (process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production"){
     //use static folder
-    app.use(express.static("./client/build"))
+    app.use(express.static("client/build"))
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"./client","build","index.html"))
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     })
 }
 
